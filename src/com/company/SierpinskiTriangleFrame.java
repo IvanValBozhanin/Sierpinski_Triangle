4d826a1cd429dccbbbe3bf5c8aab04717878d102
Ivan Valentinov <ivan156drs@gmail.com>
2021-03-02 13:00:16 +0200

    Initial commit

---
+++ src/com/company/SierpinskiTriangleFrame.java
@@ -0,0 +1,42 @@
+package com.company;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class SierpinskiTriangleFrame extends JFrame {
+    static int r = 0, gr = 0, b = 255;
+
+    private int n;
+
+    private final SierpinskiTrianglePanel trianglePanel = new SierpinskiTrianglePanel(); // To display the pattern
+    private final JLabel currOrder;
+
+    public SierpinskiTriangleFrame() {
+
+        JPanel panel = new JPanel();
+        currOrder = new JLabel("Current order: " + n);
+        panel.add(currOrder);
+
+        JButton plus = new JButton("+");
+        plus.setFont(new Font("Serif", Font.BOLD, 20));
+        panel.add(plus);
+
+        JButton minus = new JButton("-");
+        minus.setFont(new Font("Serif", Font.BOLD, 20));
+        panel.add(minus);
+
+        add(trianglePanel);
+        add(panel, BorderLayout.SOUTH);
+
+        plus.addActionListener(e -> {
+            trianglePanel.setOrder(++n);
+            currOrder.setText("Current order: " + n);
+        });
+        minus.addActionListener(e -> {
+            trianglePanel.setOrder(--n);
+            currOrder.setText("Current order: " + n);
+        });
+
+    }
+
+}
